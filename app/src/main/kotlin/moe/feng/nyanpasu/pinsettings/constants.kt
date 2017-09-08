4d0123d6fa1ca68b9bdca1e6052bac4801edfb0d
Fung <fython@163.com>
2017-09-08 17:17:17 +0800

    About dialog
    Signed-off-by: Fung <fython@163.com>

--- app/src/main/kotlin/moe/feng/nyanpasu/pinsettings/constants.kt
+++ app/src/main/kotlin/moe/feng/nyanpasu/pinsettings/constants.kt
@@ -3,3 +3,8 @@ package moe.feng.nyanpasu.pinsettings
 const val ACTION_SUCCESS_ADD = "moe.feng.nyanpasu.pinsettings.ACTION_SUCCESS_ADD"
 
 const val EXTRA_ACTION_NAME = "action_name"
+
+const val ALIPAY_TRANSFER_URL = "intent://platformapi/startapp?saId=10000007&" +
+		"clientVersion=3.7.0.0718&qrcode=https%3A%2F%2Fqr.alipay.com%2Faehvyvf4taua18zo6e%3F_s" +
+		"%3Dweb-other&_t=1472443966571#Intent;" +
+		"scheme=alipayqr;package=com.eg.android.AlipayGphone;end"
--- app/src/main/kotlin/moe/feng/nyanpasu/pinsettings/ui/MainActivity.kt
+++ app/src/main/kotlin/moe/feng/nyanpasu/pinsettings/ui/MainActivity.kt
@@ -1,10 +1,12 @@
 package moe.feng.nyanpasu.pinsettings.ui
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -13,10 +15,7 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
-import moe.feng.nyanpasu.pinsettings.ACTION_SUCCESS_ADD
-import moe.feng.nyanpasu.pinsettings.EXTRA_ACTION_NAME
-import moe.feng.nyanpasu.pinsettings.R
-import moe.feng.nyanpasu.pinsettings.SETTINGS_ITEMS
+import moe.feng.nyanpasu.pinsettings.*
 import moe.feng.nyanpasu.pinsettings.ui.adapter.ModelBindAdapter
 import moe.feng.nyanpasu.pinsettings.ui.adapter.SettingsItemBinder
 import moe.feng.nyanpasu.pinsettings.util.Preferences
@@ -76,7 +75,20 @@ class MainActivity : Activity() {
 			true
 		}
 		R.id.action_about -> {
-			// TODO About dialog
+			AlertDialog.Builder(this)
+					.setTitle(R.string.app_name)
+					.setMessage(R.string.about_message)
+					.setPositiveButton(android.R.string.ok) {_, _ -> }
+					.setNegativeButton(R.string.button_donate_via_alipay) {_, _ ->
+						val intent = Intent.parseUri(ALIPAY_TRANSFER_URL, Intent.URI_INTENT_SCHEME)
+						startActivity(intent)
+					}
+					.setNeutralButton(R.string.button_github) {_, _ ->
+						val intent = Intent(Intent.ACTION_VIEW,
+								Uri.parse("https://github.com/fython/PinSettings"))
+						startActivity(intent)
+					}
+					.show()
 			true
 		}
 		else -> super.onOptionsItemSelected(item)
